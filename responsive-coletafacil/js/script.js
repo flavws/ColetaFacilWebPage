be6fa29570de4b3c9904da96d6fb1b1a767af634
Flavs <65199855+flavws@users.noreply.github.com>
2022-04-25 16:53:43 -0300

    Add files via upload

---
+++ responsive-coletafacil/js/script.js
@@ -0,0 +1,9 @@
+var navLinks = document.getElementById("navLinks")
+
+function abrirMenu(){
+    navLinks.style.right = "0";
+ }
+
+ function fecharMenu(){
+     navLinks.style.right = "-220px";
+ }
